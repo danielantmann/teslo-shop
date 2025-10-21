a2d170e0fa9e9754509c60bdc683de550a20facf
Daniel Eric Antmann <tula3x3@gmail.com>
2025-10-21 18:39:34 +0200

    feat(admin): add ActivityFeed, Chart,Header,QuickActions, SideBar, StartCard components

--- src/admin/layouts/AdminLayouts.tsx
+++ src/admin/layouts/AdminLayouts.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+
 export const AdminLayouts = () => {
-  return <div>AdminLayouts</div>;
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex">
+      <Sidebar
+        isCollapsed={sidebarCollapsed}
+        onToggle={() => setSidebarCollapsed(!sidebarCollapsed)}
+      />
+
+      <div className="flex-1 flex flex-col">
+        <Header />
+
+        <main className="flex-1 p-6"></main>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayouts;
